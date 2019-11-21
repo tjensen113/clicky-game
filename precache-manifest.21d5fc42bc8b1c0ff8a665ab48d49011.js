d975a9aa1e0543d3ad6de27639580aecffce82ae
thomas jensen <tjensen113@gmail.com>
2019-11-21 16:58:38 -0600

    Updates

---
+++ precache-manifest.21d5fc42bc8b1c0ff8a665ab48d49011.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8c6e67d0c2fcc5d6bb080e6c0cfaf7df",
+    "url": "/clicky-game/index.html"
+  },
+  {
+    "revision": "dc3fb01971531af316f9",
+    "url": "/clicky-game/static/css/main.8cd96138.chunk.css"
+  },
+  {
+    "revision": "8a501fff1985d03bc65d",
+    "url": "/clicky-game/static/js/2.a834fa33.chunk.js"
+  },
+  {
+    "revision": "dc3fb01971531af316f9",
+    "url": "/clicky-game/static/js/main.6d297516.chunk.js"
+  },
+  {
+    "revision": "6ca587a76489ec570df8",
+    "url": "/clicky-game/static/js/runtime~main.9c1d291a.js"
+  }
+]);
